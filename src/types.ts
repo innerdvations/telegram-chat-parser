ac0d73f882491e867fb8ed28ca2c2a6faf54b912
Benjamin Irvin <ben@innerdvations.com>
2021-03-26 00:03:30 +0100

    add more data definitions from API docs

--- src/types.ts
+++ src/types.ts
@@ -1,38 +1,34 @@
-// TODO: learn more about API so we can define ExportedMessage better
-// {
-//   id:number;
-//   type:string;
-//   date:string;
-//   from:string;
-//   // eslint-disable-next-line camelcase
-//   from_id:number;
-//   text:string|[];
-// };
-
+// TODO: remove UnknownIndices once more is known about export file formats
 export type UnknownIndices = { [index:string]:unknown };
 
 export type BaseMessage = UnknownIndices & {
   id:number;
-  type:'message' | 'service';
+  type:MessageType;
   date:string;
   text:ExportedText;
 };
 
+export type MessageType = 'message' | 'service';
+
+//
+// "Message" types
+//
 export type ExportedMessage = BaseMessage & {
-  from?:string;
-  from_id?:number;
+  type:'message';
+  from:string;
+  from_id:number;
   forwarded_from?:string;
-  forwarded_from_id?:string; // never actually sent, but just in case
+  forwarded_from_id?:number; // never actually sent, but just in case
   saved_from?:string;
-  saved_from_id?:string; // never actually sent, but just in case
+  saved_from_id?:number; // never actually sent, but just in case
   via_bot?:string;
-  poll?:Poll;
   reply_to_message_id?:number;
 };
 
+// Sent images are mediamessage with mime_type like "image/png" etc
 export type MediaMessage = ExportedMessage & {
   file?:string;
-  media_type?:string;
+  media_type?:MediaType;
   mime_type?:string;
   duration_seconds?:string;
   width?:number;
@@ -41,20 +37,90 @@ export type MediaMessage = ExportedMessage & {
   thumbnail?:string;
 };
 
-export type ServiceMessage = BaseMessage & {
-  actor?:string;
-  actor_id?:number;
-  action?:ServiceAction;
+// Untested, but this may be for photos taken within app.
+export type PhotoMessage = ExportedMessage & {
+  photo:string;
+  width:number;
+  height:number;
+};
+
+// Can't find a list of these in the API anywhere, but this seems to be it from testing
+export type MediaType = 'animation' | 'sticker' | 'video_file' | 'voice_message';
+
+export type PollMessage = ExportedMessage & {
+  poll:Poll;
+};
+
+export type Poll = {
+  question:string;
+  close:boolean;
+  total_voters:number;
+  answers:PollAnswer[];
+};
+
+export type PollAnswer = {
+  text:string;
+  voters:number;
+  chosen:boolean;
+};
+
+// From inputMedia* at https://core.telegram.org/schema
+
+// Types that exist in telegram API but not exported by Telegram Desktop:
+// - dice
+
+// TODO: Test and add the following media types:
+// - geo_live
+// - invoice
+// - game
+// - document_external
+// - photo_external
+// - gif_external
+// - venue
+// - document
+// - uploaded_document
+// - contact
+// - geo_point
+// - uploaded_photo
+// - empty
+
+//
+// "Service" types
+//
+export type ExportedService = BaseMessage & {
+  type:'service';
+  actor:string;
+  actor_id:number;
+  action:Action;
   message_id?:number;
 };
 
-export type AnyMessage = ExportedMessage & MediaMessage & ServiceMessage;
+// TODO: define types for these
+// https://core.telegram.org/type/MessageAction
+export type Action =
+  'empty' |
+  'chat_create' | 'chat_edit_title' | 'chat_edit_photo' | 'chat_delete_photo' |
+  'chat_add_user' | 'chat_delete_user' | 'chat_joined_by_link' | 'chat_migrate_to' |
+  'channel_create' | 'channel_migrate_from' |
+  'pin_message' | 'history_clear' | 'game_score' |
+  'payment_sent_me' | 'payment_sent' |
+  'phone_call' | 'screenshot_taken' | 'custom_action' | 'bot_allowed' |
+  'secure_values_sent_me' | 'secure_values_sent' |
+  'contact_sign_up';
+
+// List all message types above
+export type AnyMessage =
+  ExportedMessage &
+  MediaMessage &
+  PollMessage &
+  PhotoMessage &
+  ExportedService;
 
 export type ChatExport = UnknownIndices & {
   id:number;
   name:string;
   type:string;
-  messages:ExportedMessage[];
+  messages:AnyMessage[];
 };
 
 export type ImportMessageOptions = {
@@ -73,18 +139,12 @@ export type TextObject = {
   text:string,
 };
 
-// TODO: fill this in with all known service actions
-export type ServiceAction = 'pin_message';
+//
+// Service definitions
+//
+export type PhoneCall = {
+  duration_seconds:number,
 
-export type Poll = {
-  question:string;
-  close:boolean;
-  total_voters:number;
-  answers:PollAnswers;
-};
-
-export type PollAnswers = {
-  text:string;
-  voters:number;
-  chosen:boolean;
+  // https://core.telegram.org/type/PhoneCallDiscardReason
+  discard_reason:'hangup' | 'disconnect' | 'busy' | 'missed',
 };
