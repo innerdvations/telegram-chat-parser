fec97ad62d1cd9708ade49a395d12346c1521cf2
Benjamin Irvin <ben@innerdvations.com>
2021-03-25 09:16:39 +0100

    split users tests into own file

--- tests/TelegramChat.spec.ts
+++ tests/TelegramChat.spec.ts
@@ -4,7 +4,7 @@ import * as fs from 'fs';
 import chaiLike from 'chai-like';
 import moment from 'moment';
 import {
-  ContentType, TelegramChat, TelegramMessage, TelegramUser,
+  ContentType, TelegramChat, TelegramMessage,
 } from '../src';
 
 chai.use(chaiLike);
@@ -22,9 +22,6 @@ const PublicSupergroupJSON = fs.readFileSync('./tests/data/public_supergroup.jso
 
 const SavedJSON = fs.readFileSync('./tests/data/saved.json', { encoding: 'utf8', flag: 'r' });
 const SavedObj = JSON.parse(SavedJSON);
-
-const UsersTestJSON = fs.readFileSync('./tests/data/users_test.json', { encoding: 'utf8', flag: 'r' });
-// const UsersTestObj = JSON.parse(UsersTestJSON);
 
 describe('TelegramChat', () => {
   describe('when importing invalid json', () => {
@@ -74,54 +71,6 @@ describe('TelegramChat', () => {
       });
     });
 
-    describe('participants', () => {
-      const tgUsersTest = new TelegramChat(UsersTestJSON);
-      const { participants } = tgUsersTest;
-      it('should have correct amount of users', () => {
-        expect(participants).to.have.length(3);
-      });
-    });
-
-    describe('users', () => {
-      const tgUsersTest = new TelegramChat(UsersTestJSON);
-      const { users } = tgUsersTest;
-      it('should have correct amount of users', () => {
-        expect(users).to.have.length(4);
-      });
-      it('should find user first found in from with correct data', () => {
-        const user = users.find((u) => u.name === 'User1');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).name).to.equal('User1');
-        expect((user as TelegramUser).id).to.equal(1);
-      });
-      it('should find user first found in forwarded_from with correct name', () => {
-        const user = users.find((u) => u.name === 'User3');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).name).to.equal('User3');
-      });
-      it('should find user first found in saved_from with correct name', () => {
-        const user = users.find((u) => u.name === 'User4');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).name).to.equal('User4');
-      });
-      it('should not have id for user only found in saved_from', () => {
-        const user = users.find((u) => u.name === 'User4');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).id).to.be.undefined;
-      });
-      it('should have name and id for user initially found without id', () => {
-        const user = users.find((u) => u.name === 'User3');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).name).to.equal('User3');
-        expect((user as TelegramUser).id).to.equal(3);
-      });
-      it('should know if a user is a participant', () => {
-        const user = users.find((u) => u.name === 'User3');
-        expect(user).to.not.be.undefined;
-        expect((user as TelegramUser).participated).to.be.true;
-      });
-    });
-
     describe('messages', () => {
       it('should all exist', () => {
         expect(tg.messages).to.have.length(SavedObj.messages.length);
@@ -140,38 +89,6 @@ describe('TelegramChat', () => {
       });
       it('should have correct type', () => {
         expect(tg.messages[0].type).to.equal(SavedObj.messages[0].type);
-      });
-
-      // TODO: break this up into individual user functions? Pending refactoring of that code.
-      describe('user functions', () => {
-        it('should have correct forwarded_from user', () => {
-          const data = SavedObj.messages.find((m:AnyMessage) => m.forwarded_from !== undefined);
-          const msg = tg.messageByID(data.id) as TelegramMessage;
-          expect((msg.forwardedFrom as TelegramUser).name).to.equal(data.forwarded_from);
-        });
-        it('should have correct saved_from user', () => {
-          const data = SavedObj.messages.find((m:AnyMessage) => m.saved_from !== undefined);
-          const msg = tg.messageByID(data.id) as TelegramMessage;
-          expect((msg.savedFrom as TelegramUser).name).to.equal(data.saved_from);
-        });
-        it('should have correct from user', () => {
-          const data = SavedObj.messages.find((m:AnyMessage) => m.from !== undefined);
-          const msg = tg.messageByID(data.id) as TelegramMessage;
-          expect((msg.from as TelegramUser).id).to.eql(data.from_id);
-          expect((msg.from as TelegramUser).name).to.eql(data.from);
-        });
-        it('should have correct actor user', () => {
-          const data = SavedObj.messages.find((m:AnyMessage) => m.actor !== undefined);
-          const msg = tg.messageByID(data.id) as TelegramMessage;
-          expect((msg.actor as TelegramUser).id).to.eql(data.actor_id);
-          expect((msg.actor as TelegramUser).name).to.eql(data.actor);
-        });
-        // TODO: Even though this gets 100% coverage, it needs to test all the user functions
-        it('should return undefined when not present', () => {
-          const data = SavedObj.messages.find((m:AnyMessage) => m.from && !m.actor);
-          const msg = tg.messageByID(data.id) as TelegramMessage;
-          expect((msg.actor)).to.be.undefined;
-        });
       });
 
       describe('replyTo', () => {
